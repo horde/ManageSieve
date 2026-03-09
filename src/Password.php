e12affa5486dd2a033b8fac6e17aadce5e479a5e
www-data <www-data@u-picardie.fr>
2026-03-09 15:08:54 +0100

    feat(auth): add XOAUTH2 authentication support

---
+++ src/Password.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Copyright 2025 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (BSD). If you
+ * did not receive this file, see http://www.horde.org/licenses/bsd.
+ *
+ * @category  Horde
+ * @copyright 2025 Horde LLC
+ * @license   http://www.horde.org/licenses/bsd BSD
+ * @package   ManageSieve
+ */
+
+namespace Horde\ManageSieve;
+
+/**
+ * Interface representing a ManageSieve password object.
+ *
+ * @author    Jean Charles Delepine <delepine@u-picardie.fr>
+ * @category  Horde
+ * @copyright 2026 Horde LLC
+ * @license   http://www.horde.org/licenses/bsd BSD
+ * @package   ManageSieve
+ * @since     2.0.0
+ */
+interface Password
+{
+    /**
+     * Return the password to use for the server connection.
+     *
+     * @return string  The password.
+     */
+    public function getPassword();
+}
